0c7172d5ff9b896fa1235756edd53401b739be0a
Nyelus101 <cmuonyelu@gmail.com>
2023-02-12 11:19:45 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Returns the available attributes and methods of an object"""
+    return dir(obj)
