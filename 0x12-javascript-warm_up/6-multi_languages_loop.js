5c73a3cb0fa5ad67e025ea0897e9502138c004c5
root <root@LAPTOP-HNTNEVRF>
2023-04-24 03:05:35 +0100

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myVar0 = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'Javascript is amazing';
+const arr = [myVar0, myVar1, myVar2];
+for (let i = 0; i < arr.length; i = i + 1) {
+  console.log(arr[i]);
+}
