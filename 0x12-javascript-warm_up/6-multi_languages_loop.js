2830ef1b8db51a77feec228356d0f9a9b08953a2
root <root@LAPTOP-HNTNEVRF>
2023-04-24 03:11:16 +0100

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const myVar0 = 'C is fun';
-const myVar1 = 'Python is cool';
-const myVar2 = 'Javascript is amazing';
-const arr = [myVar0, myVar1, myVar2];
-for (let i = 0; i < arr.length; i = i + 1) {
-  console.log(arr[i]);
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+
+while (langs[i]) {
+  console.log(langs[i]);
+  i++;
 }
